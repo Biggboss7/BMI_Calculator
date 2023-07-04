46238204a42c3edf44ef17b970e878c2bfae660e
Michael <mtse431@gmail.com>
2023-07-04 14:54:45 +1000

    Creates Functions in JS to Calculate BMI

--- index.js
+++ index.js
@@ -10,16 +10,43 @@ const minBMI = 18.5;
 const maxBMI = 24.9;
 
 const usersInfo = {
-    heightInCM: null,
-    weightInKG: null,
+    heightInCM: 0,
+    weightInKG: 0,
+    heightInFeet: 0,
+    heightInInch: 0,
+    weightInSt: 0,
+    weightInLbs: 0,
+    isMetric: true,
     calculateMetricBMI: function () {
         const heightToMeter = this.heightInCM / 100;
         return (this.weightInKG / Math.pow(heightToMeter, 2)).toFixed(1); // return: str
+    },
+    calculateImperialBMI: function () {
+        const totalInches = (this.heightInFeet * 12) + this.heightInInch;
+        const totalLbs = (this.weightInSt * 14) + this.weightInLbs;
+        return (totalLbs / Math.pow(totalInches, 2) * 703).toFixed(1); // return str
     },
     idealWeightinKG: function () {
         const minWeight = (Math.pow(this.heightInCM / 100, 2) * minBMI).toFixed(1);
         const maxWeight = (Math.pow(this.heightInCM / 100, 2) * maxBMI).toFixed(1);
         return [minWeight, maxWeight]; // return: [str, ...]
+    },
+    idealWeightImperial: function () {
+        const totalMinWeight = (Math.pow(((this.heightInFeet * 12) + this.heightInInch), 2) * minBMI / 703);
+        const totalMaxWeight = (Math.pow(((this.heightInFeet * 12) + this.heightInInch), 2) * maxBMI / 703);
+        const minWeightSt = Math.floor((totalMinWeight / 14));
+        const minWeightLbs = Math.floor((totalMinWeight % 14));
+        const maxWeightSt = Math.floor((totalMaxWeight / 14));
+        const maxWeightLbs = Math.floor((totalMaxWeight % 14));
+        return [minWeightSt, minWeightLbs, maxWeightSt, maxWeightLbs];
+    },
+    resetInput: function () {
+        for (const el of numberInputList) {
+            el.value = "";
+            usersInfo[el.id] = 0;
+        }
+        document.querySelector("#greeting").innerHTML = "Welcome!";
+        resultSummaryContainerEl.classList.remove("activated");
     }
 };
 
@@ -30,7 +57,12 @@ function bmiDescription(bmi) {
     else if (bmi <= 29.9) bmiStatus = "over weight";
     else if (bmi <= 34.9) bmiStatus = "obese";
     else bmiStatus = "extremely obese";
-    return `Your BMI suggests you're a <strong>${bmiStatus}</strong>. Your ideal weight is between <strong>${usersInfo.idealWeightinKG()[0]}kgs - ${usersInfo.idealWeightinKG()[1]}kgs.</strong>`;
+
+    if (usersInfo.isMetric)
+        return `Your BMI suggests you're a <strong>${bmiStatus}</strong>. Your ideal weight is between <strong>${usersInfo.idealWeightinKG()[0]}kgs - ${usersInfo.idealWeightinKG()[1]}kgs.</strong>`;
+    else
+        return `Your BMI suggests you're a <strong>${bmiStatus}</strong>. Your ideal weight is between <strong>${usersInfo.idealWeightImperial()[0]}st ${usersInfo.idealWeightImperial()[1]}lbs - ${usersInfo.idealWeightImperial()[2]}st ${usersInfo.idealWeightImperial()[3]}lbs.</strong>`;
+
 }
 
 for (const element of numberInputList) {
@@ -39,9 +71,18 @@ for (const element of numberInputList) {
             const whatInfo = e.target.id;
             const infoId = `#${e.target.id}`;
             usersInfo[whatInfo] = document.querySelector(infoId).valueAsNumber;
+            // Metric Scale
             if (usersInfo.heightInCM && usersInfo.weightInKG) {
                 resultSummaryContainerEl.classList.add("activated");
                 const bmiScore = usersInfo.calculateMetricBMI();
+                document.querySelector("#greeting").innerHTML = "Your BMI is ...";
+                usersBMIScoreEl.innerHTML = bmiScore;
+                usersBMISummary.innerHTML = bmiDescription(Number(bmiScore));
+            };
+            // Imperial Scale
+            if (usersInfo.heightInInch && usersInfo.weightInLbs) {
+                resultSummaryContainerEl.classList.add("activated");
+                const bmiScore = usersInfo.calculateImperialBMI();
                 document.querySelector("#greeting").innerHTML = "Your BMI is ...";
                 usersBMIScoreEl.innerHTML = bmiScore;
                 usersBMISummary.innerHTML = bmiDescription(Number(bmiScore));
@@ -55,8 +96,12 @@ for (const radio of radioInputList) {
         function (e) {
             const lastClass = bmiForm.classList[1];
             const chosenRadio = e.target.id;
-            bmiForm.classList.remove(lastClass);
-            bmiForm.classList.add(chosenRadio);
+            chosenRadio === "metric" ? usersInfo.isMetric = true : usersInfo.isMetric = false;
+            if (chosenRadio !== lastClass) {
+                bmiForm.classList.remove(lastClass);
+                bmiForm.classList.add(chosenRadio);
+                usersInfo.resetInput();
+            }
         })
 }
 
